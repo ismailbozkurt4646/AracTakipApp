f8669cc37205dcfb9bdb87da7ed862dbe7f96830
ismailbozkurt4646 <kumdankale_20@hotmail.com>
2026-01-20 13:50:50 +0300

    port dinleme eklendi

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    # Render portu dinamik olarak atar, bulamazsa varsayılan 5000'i kullanır
+    port = int(os.environ.get("PORT", 5000))
+    # host="0.0.0.0" dış erişim için kritiktir
+    app.run(host="0.0.0.0", port=port)
